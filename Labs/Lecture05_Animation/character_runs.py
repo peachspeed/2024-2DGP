f9417c7b54c5659ce56cb70fb4e25a87b344453e
peachspeed@note <peachspeed04@gmail.com>
2024-09-30 14:34:11 +0900

    run 완성

--- Labs/Lecture05_Animation/character_runs.py
+++ Labs/Lecture05_Animation/character_runs.py
@@ -5,7 +5,14 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
-# fill here
+frame = 0
+for x in range(0, 800, 10):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame*100, 0, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame +1)%8
+    delay(0.05)
 
 
 close_canvas()
